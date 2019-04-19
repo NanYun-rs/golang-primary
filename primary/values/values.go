1bc784886fa0b1763c8f987f0a5f61df0314e9b6
yushengyu <806836039@qq.com>
2019-04-19 23:21:40 +0800

    feat(values.go): 了解golang 基本数据类型
    了解golang 基本数据类型

---
+++ primary/values/values.go
@@ -0,0 +1,13 @@
+package main
+import (
+	"fmt"
+)
+
+func main()  {
+	fmt.Println("hello" + "golang")
+	fmt.Println("1+1",1+1)
+	fmt.Println("7.2/2",7.2/2)
+	fmt.Println(true && false)
+	fmt.Println(true || false)
+	fmt.Println(!true)
+}
