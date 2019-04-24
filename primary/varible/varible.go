2f9d97a4536149a12fffc10ecc69095d64443ff0
yushengyu <806836039@qq.com>
2019-04-24 22:11:45 +0800

    feat(varible.go): go 基础数据类型
    go 基础数据类型

---
+++ primary/varible/varible.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	const a, b int = 1, 2
+	fmt.Println(a, b)
+	const c bool = false
+	fmt.Println(c)
+	var d int
+	fmt.Println(d)
+	e := 12
+	fmt.Println(e)
+}
