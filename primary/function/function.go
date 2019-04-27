da5dabf9d2f16febdacbea2086a17d828a800c05
yushengyu <806836039@qq.com>
2019-04-27 17:46:30 +0800

    feat: 更新练习文件
    更新练习文件

---
+++ primary/function/function.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+)
+
+
+func add(x int,y int) int {
+	return x+y
+}
+
+
+func sub(x,y int)int  {
+	return x - y
+}
+
+func main()  {
+	fmt.Println(add(1,2))
+	fmt.Println(sub(3,2))
+}
